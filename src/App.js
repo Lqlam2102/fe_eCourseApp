5cf709d0475ef2cb972debd790768d6448f11cde
Le Lam <lqlam28@gmail.com>
2022-06-14 20:46:23 +0700

    remove trash files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Body from './layouts/Body';
 import { ParallaxProvider } from 'react-scroll-parallax';
-import Capcha from './pages/Capcha.js'
-// import ReCAPTCHA from "react-google-recaptcha";
 
 function App() {
   return (
     <ParallaxProvider>
       <Body></Body>
-      {/* <Capcha/> */}
     </ParallaxProvider>
   );
 }
